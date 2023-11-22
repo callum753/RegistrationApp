968a1f31b6126e73336ba6e5b95e2d8ea881909b
callum753 <c0013851@hallam.shu.ac.uk>
2023-11-22 15:29:31 +0000

    done css (base.html and styles.css)

--- ModuleRegisrationSystem/views.py
+++ ModuleRegisrationSystem/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'ModuleRegisrationSystem/home.html')
+    return render(request, 'ModuleRegisrationSystem/home.html', {'title':'Welcome'})
 def about(request):
-    return render(request, 'ModuleRegisrationSystem/about.html')
+    return render(request, 'ModuleRegisrationSystem/about.html', {'title':'About us'})
 def contact(request):
-    return render(request, 'ModuleRegisrationSystem/contact.html')
+    return render(request, 'ModuleRegisrationSystem/contact.html',{'title}':'Contact us'})
 def modules (request):
-    return render(request, 'ModuleRegisrationSystem/modules.html')
+    return render(request, 'ModuleRegisrationSystem/modules.html', {'title':'modules'})
     
 
 
