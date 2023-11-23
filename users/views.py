bc3f2e5f1388da8122df336efdedec5b88f4aaa7
callum753 <c0013851@hallam.shu.ac.uk>
2023-11-23 13:35:42 +0000

    created the user Registration page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+        else:
+            messages.warning(request, 'Unable to create account!')
+        return redirect('ModuleRegisrationSystem:home')
+    else:
+        form = UserCreationForm()
+        return render(request, 'users/register.html', {'form': form , 'title': 'Module Registration'})
 
 # Create your views here.
