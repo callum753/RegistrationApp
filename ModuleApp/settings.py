65c73c0fc38022ae84768a59e99e1a72b36e6c72
callum753 <c0013851@hallam.shu.ac.uk>
2023-11-28 17:14:17 +0000

    lets user register with addation fields

--- ModuleApp/settings.py
+++ ModuleApp/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ModuleRegisrationSystem.apps.ModuleregisrationsystemConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
