67d7b66524fcccdd03e57c4ca250880fbbbfbe67
callum753 <c0013851@hallam.shu.ac.uk>
2023-11-30 13:42:52 +0000

    change defult.png to the photo name

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
-    image = models.ImageField(default = 'default.png', upload_to = 'profile_pics')
+    image = models.ImageField(default = 'pexels-photo-771742:webp', upload_to = 'profile_pics')
 
 def __str__(self):
     return f'{self.user.first_name} {self.user.last_name}'
