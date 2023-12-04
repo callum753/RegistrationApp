ae2f90592babf035f3c2b190427ce9eee4f681c3
callum753 <c0013851@hallam.shu.ac.uk>
2023-12-04 19:23:23 +0000

    problem fixed with photo and profile view

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(label = 'Email address')
@@ -15,3 +16,19 @@ class UserRegisterForm(UserCreationForm):
 class Meta:
     model = User
     fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2' , 'Birthday' , 'Address' , 'City' , 'Town' , 'Country']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    Birthday = forms.DateField()
+    Address = forms.CharField()
+    City = forms.CharField()
+    Town = forms.CharField()
+    Country = forms.CharField()
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email' , 'Birthday' , 'Address' , 'City' , 'Town' , 'Country']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
-    image = models.ImageField(default = 'pexels-photo-771742:webp', upload_to = 'profile_pics')
+    image = models.ImageField(default = 'pexels-photo-771742.webp', upload_to = 'profile_pics')
 
 def __str__(self):
     return f'{self.user.first_name} {self.user.last_name}'
